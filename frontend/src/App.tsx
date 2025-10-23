1df3396a28d8bd5e2363e3b2e489af19759ce8a7
Santiago0t <santig0talvaro@gmail.com>
2025-10-23 13:10:51 -0500

    Nuevo LuxSense

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LandingPage from "./components/pages/LandingPage";
-import DashboardPage from "./components/pages/DashboardPage";
-import LoginPage from "./components/pages/LoginPage";
-import SignupPage from "./components/pages/SignupPage";
-import WhoWeArePage from "./components/pages/WhoWeArePage";
-import ContactPage from "./components/pages/ContactPage";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage/>}/>
-        <Route path="/dashboard" element={<DashboardPage/>}/>
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/signup" element={<SignupPage/>}/>
-        <Route path="/WhoWeAre" element={<WhoWeArePage/>}/>
-        <Route path="/Contact" element={<ContactPage/>}/>
         {/*Aqui van el resto de las rutas */}
       </Routes>
     </Router>
